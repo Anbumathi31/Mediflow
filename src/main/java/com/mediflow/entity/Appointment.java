8158c5bc21ffa03912e102117cefb743bc155860
Anbumathi31 <anbumathi312327@gmail.com>
2026-06-19 09:47:49 +0530

    Add appointment entity and repository

---
+++ src/main/java/com/mediflow/entity/Appointment.java
@@ -0,0 +1,21 @@
+package com.mediflow.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long appointmentId;
+
+    private String appointmentDate;
+
+    @ManyToOne
+    private Patient patient;
+
+    @ManyToOne
+    private Doctor doctor;
+}
